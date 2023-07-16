8d98cbf227f932ddbb654c6c8d05ee22e61d8a2a
Savvy48 <viraj.patel482@gmail.com>
2023-07-16 23:51:10 +0530

    added volume control functionality

--- assets/popup.js
+++ assets/popup.js
@@ -1,1 +1,37 @@
-/* JS for popup.html */
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const volumeSlider = document.querySelector(".slider");
+    const volumeDisplay = document.querySelector(".current-val");
+
+    function setVolume(volume) {
+        const mappedVolume = volume / 200;
+        volumeDisplay.textContent = volume.toFixed(0);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: tabs[0].id },
+                    function: setVideoVolume,
+                    args: [mappedVolume],
+                },
+                function () {
+                    if (chrome.runtime.lastError) {
+                        console.error(chrome.runtime.lastError.message);
+                    }
+                }
+            );
+        });
+    }
+
+    function setVideoVolume(volume) {
+        const video = document.querySelector("video");
+        if (video) {
+            video.volume = volume;
+        }
+    }
+
+    volumeSlider.addEventListener("input", function () {
+        const volumeValue = parseInt(this.value);
+        setVolume(volumeValue);
+    });
+    
+});
