f4e67493638b448e649d7847ce8f96806bfe4e9b
Savvy48 <viraj.patel482@gmail.com>
2023-07-21 21:49:45 +0530

    fixed volume functionality

--- assets/popup.js
+++ assets/popup.js
@@ -1,37 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     const volumeSlider = document.querySelector(".slider");
     const volumeDisplay = document.querySelector(".current-val");
 
-    function setVolume(volume) {
-        const mappedVolume = volume / 200;
-        volumeDisplay.textContent = volume.toFixed(0);
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript(
-                {
-                    target: { tabId: tabs[0].id },
-                    function: setVideoVolume,
-                    args: [mappedVolume],
-                },
-                function () {
-                    if (chrome.runtime.lastError) {
-                        console.error(chrome.runtime.lastError.message);
-                    }
-                }
-            );
-        });
-    }
-
-    function setVideoVolume(volume) {
-        const video = document.querySelector("video");
-        if (video) {
-            video.volume = volume;
-        }
-    }
-
     volumeSlider.addEventListener("input", function () {
         const volumeValue = parseInt(this.value);
-        setVolume(volumeValue);
+        const multiplier = volumeValue / 100;
+
+        volumeDisplay.textContent = volumeValue;
+        chrome.runtime.sendMessage({
+            message: 'pass_to_background',
+            value: multiplier
+        });
     });
-    
 });
