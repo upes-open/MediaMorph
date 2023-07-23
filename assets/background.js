107d4628ef5fd6e76c88a85261c27b1cfc9b47ee
Savvy48 <viraj.patel482@gmail.com>
2023-07-23 15:49:17 +0530

    Added video speed functionality

--- assets/background.js
+++ assets/background.js
@@ -17,4 +17,38 @@ chrome.runtime.onMessage.addListener((request) => {
             }
         });
     }
+
+    if (request.message === 'speed_up_video' && typeof request.value === 'number') {
+        const speed = request.value;
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            if (activeTab) {
+                chrome.tabs.sendMessage(activeTab.id, {
+                    message: 'speed_up_video',
+                    value: speed
+                })
+                    .then(console.log('Sent to content.js:'))
+                    .catch(error => console.error('Error sending message to content.js:', error));
+            } else {
+                console.error('No active tab found.');
+            }
+        });
+    }
+
+    if (request.message === 'speed_down_video' && typeof request.value === 'number') {
+        const speed = request.value;
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            if (activeTab) {
+                chrome.tabs.sendMessage(activeTab.id, {
+                    message: 'speed_down_video',
+                    value: speed
+                })
+                    .then(console.log('Sent to content.js:'))
+                    .catch(error => console.error('Error sending message to content.js:', error));
+            } else {
+                console.error('No active tab found.');
+            }
+        });
+    }
 });
